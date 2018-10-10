18b700bac57355587c3410d51c66a03e8af68d08
Alan Vieceli <alansvieceli@gmail.com>
2018-10-10 12:54:08 -0300

    Validando dados de entrada com Bean Validation

--- src/main/java/com/alanvieceli/api/controllers/EmpresaController.java
+++ src/main/java/com/alanvieceli/api/controllers/EmpresaController.java
@@ -1,6 +1,9 @@
 package com.alanvieceli.api.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,8 +17,13 @@ import com.alanvieceli.api.response.Response;
 public class EmpresaController {
 
 	@PostMapping
-	public ResponseEntity<Response<EmpresaDto>> cadastrar(@RequestBody EmpresaDto empresaDto) {
+	public ResponseEntity<Response<EmpresaDto>> cadastrar(@Valid @RequestBody EmpresaDto empresaDto, BindingResult result) {
 		Response<EmpresaDto> response = new Response<EmpresaDto>();
+		
+		if (result.hasErrors()) {
+			result.getAllErrors().forEach(error -> response.getErros().add(error.getDefaultMessage()));
+			return ResponseEntity.badRequest().body(response);
+		}
 		
 		empresaDto.setId(1L);
 		response.setData(empresaDto);
